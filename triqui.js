957537f2e6689423e097552b8ebe98efb1ed7d40
Kandresdiaz <kevina0416@gmail.com>
2023-02-13 02:11:20 -0500

    first commit

---
+++ triqui.js
@@ -0,0 +1,39 @@
+// alert(" eligue tu opcion X, O")
+const container = document.getElementById('container');
+let casilla = document.querySelectorAll('.casilla');
+
+let opciones= document.querySelectorAll('.opciones')
+
+opciones.forEach(element=>{
+    let opc =[...opciones]
+    element.addEventListener('click',()=>{
+        if(element== opc[0]){  
+            casilla.forEach(element1=>{
+                element1.addEventListener('click',()=>{
+                    element1.innerHTML = '0'
+                   })
+            })
+            element.style.background="green"
+            
+        }
+        else{
+            element.style.background="blue"
+            casilla.forEach(element2=>{
+                element2.addEventListener('click',()=>{
+                    element2.innerHTML = 'x'
+                   })
+            })
+        }
+    })
+
+})
+console.log(casilla);
+
+for(let i=0;i<3;i++){
+    if(casilla==casilla){
+        alert('ganador')
+    }
+}
+   
+
+    
